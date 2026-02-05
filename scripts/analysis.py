0688fdb01de13a2ab8223380969c5095078829f3
Sara-Soares-de-Oliveira <sarasoares99@outlook.com>
2026-02-05 02:26:10 +0000

    Enhance analysis script with brute-force attack detection

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -5,12 +5,33 @@ import pandas as pd
 
 file = pd.read_csv('./results/log_data.csv')
 
+# ==========================================
+# SECTION : Findings  
+# ==========================================
+
 #top 10 ips 
 rhost = file["rhost"].value_counts().head(10)
-print (rhost)
+
 
 #top 10 users
 user = file["user"].value_counts().head(10)
-print (user)
 
-# Hours with the most attack occourrencies 
+
+
+# ==========================================
+# SECTION : Brute-force attack detection
+# ==========================================
+
+# Brute-force attack detection
+ 
+ # Get the rhost collum and filter out the null values   
+filtered = file.dropna(subset=["rhost"])
+
+# New column with floor minutes 
+time = "2026 " + filtered["time"]
+formated_date = pd.to_datetime(time, format="%Y %b %d %H:%M:%S")
+floor_minutes =(formated_date.dt.floor('min'))
+
+filtered.insert(1, 'floor_minutes', floor_minutes)
+
+print(filtered.head(3)) 
